6dec773a292a3b8b6b6f69d0c6762435bd68e484
Krishna <35102612+kvdevel@users.noreply.github.com>
2022-10-12 13:46:53 +0530

    Moved charts to src
    Signed-off-by: Krishna <35102612+kvdevel@users.noreply.github.com>

---
+++ src/package.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -x
+
+PKG=${1:-"all"}
+
+if [ "$PKG" = "all" ]; then 
+	PKG="alertaction madhava nodewebserver partha postgresdb shyama"
+fi
+
+helm package --dependency-update --destination ../docs/ $PKG
+
+if [ $? -ne 0 ]; then
+	echo -e "\nERROR : Helm Package creation failed...\n\n"
+	exit 1
+fi
+
+helm repo index --url https://gyeeta.github.io/helmcharts ../docs/
+
+if [ $? -ne 0 ]; then
+	echo -e "\nERROR : Helm repo index creation failed...\n\n"
+	exit 1
+fi
+
+exit 0
+
